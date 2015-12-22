f8f4e83b3a2690eefc683c7fdaf6ef239283c177
Michael Entrup (b. Epping) <mail@m-entrup.de>
2015-12-22 22:49:23 +0100

    .jshintrc nutzen:
    In Gruntfile.js ist nun die Nutzung von .jshintrc aktiviert.
    dadurch sind mir noch ein paar Fehler aufgefallen, die jetzt behoben sind.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,26 @@
+/* global module, require */
+
 module.exports = function(grunt) {
+  "use strict";
   // https://www.npmjs.com/package/grunt-contrib-jshint
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks("grunt-contrib-jshint");
   // https://www.npmjs.com/package/grunt-contrib-watch
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks("grunt-contrib-watch");
 
   // Konfiguration der Aufgaben:
   grunt.initConfig({
     jshint: {
-      all: ['Gruntfile.js']
+      options: {
+        jshintrc: true,
+        reporter: require("jshint-stylish"),
+      },
+      all: ["Gruntfile.js", "flightplan.js", "js/*.js"],
     },
 
     watch: {
       scripts: {
-        files: ['**/*.js'],
-        tasks: ['jshint'],
+        files: ["Gruntfile.js", "flightplan.js", "js/*.js"],
+        tasks: ["jshint"],
         options: {
           spawn: false,
         },
@@ -22,8 +29,8 @@ module.exports = function(grunt) {
   });
 
   // Diese Aufgabe wird beim Aufruf ohne Parameter ausgeführt.
-  grunt.registerTask('default', ['jshint', 'watch']);
+  grunt.registerTask("default", ["jshint", "watch"]);
 
   // Diese Aufgabe wird von flightplan aufgerufen.
-  grunt.registerTask('prefly', []);
+  grunt.registerTask("prefly", []);
 };
--- flightplan.js
+++ flightplan.js
@@ -1,35 +1,39 @@
+/* global require, process */
+
 // https://www.npmjs.com/package/flightplan
 // https://github.com/pstadler/flightplan
-var plan = require('flightplan');
+var plan = require("flightplan");
 
 //Konfiguration
-plan.target('publish', {
+plan.target("publish", {
   // Mein Webspace ist bei Strato, weshalb ich diese URL für ssh nutzen muss
-  host: 'ssh.strato.de',
+  host: "ssh.strato.de",
   // Bei Strato ist die Domain der Nutzername
-  username: 'm-entrup.de',
+  username: "m-entrup.de",
   agent: process.env.SSH_AUTH_SOCK
 });
 
 // Hat man mehrere Domains, dann sind die Dateien zu diesen sehr wahrscheinlich in Unterordnern
-var remoteDir = 'bootstrap/m-entrup/'
-// var remoteDir = 'example-com-' + new Date().getTime();
+var remoteDir = "bootstrap/m-entrup/";
+// var remoteDir = "example-com-" + new Date().getTime();
 
 // Die folgenden Befehle werden in der angegebenen Reihenfolge ausgeführt.
 // Man kann weitere Aufrufe von plan.remote() und plan.local() hinzufügen.
 
 // Befehle, welche auf dem Server ausgeführt werden
 plan.remote(function(remote) {
-  remote.log('Create folder to copy files to');
-  remote.exec('mkdir -p ' + remoteDir);
+  "use strict";
+  remote.log("Create folder to copy files to");
+  remote.exec("mkdir -p " + remoteDir);
 });
 
 // Befehle, welche lokal ausgeführt werden
 plan.local(function(local) {
-  local.log('Run build');
-  local.exec('grunt prefly');
-  local.log('Copy files to remote host');
-  var filesToCopy = local.exec('git ls-files', {silent: true});
-  // rsync files to all the target's remote hosts
+  "use strict";
+  local.log("Run build");
+  local.exec("grunt prefly");
+  local.log("Copy files to remote host");
+  var filesToCopy = local.exec("git ls-files", {silent: true});
+  // rsync files to all the target"s remote hosts
   local.transfer(filesToCopy, remoteDir);
 });
